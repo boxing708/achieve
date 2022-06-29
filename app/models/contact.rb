d7e5c6ba747eae6bf4740d1a21275794abf754be
boxing708 <naoya.shishikura@super-studio.jp>
2022-06-29 18:57:55 +0900

    Add category validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,6 @@
 class Contact < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
+  validates :category, presence: true
   validates :content, presence: true, length: { maximum: 140 }
 end
